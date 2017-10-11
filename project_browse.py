947999680dcbdc1a509fceda155c1706d6aaff6e
Bartek Nuszkiewicz <barteknuszkiewicz@gmail.com>
2017-10-11 23:17:47 +0200

    Updates

--- project_browse.py
+++ project_browse.py
@@ -6,11 +6,29 @@ import requests, os, bs4
 url = 'http://xkcd.com'              # starting url
 os.makedirs('xkcd', exist_ok=True)   # store comics in ./xkcd
 while not url.endswith('#'):
-    # TODO: Download the page.
+    #Download the page.
+    print('Downloading page {}'.format(url))
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-    # TODO: Find the URL of the comic image.
 
-    # TODO: Download the image.
+    #Find the URL of the comic image.
+    comicElem = soup.select('#comic img')
+    if comicElem == []:
+        print('Could not find comic image')
+    else:
+        try:
+            comicUrl = 'http:' + comicElem[0].get('src')
+            #Download the image.
+            print('Downloading image {}'.format(comicUrl))
+            res = request.get(comicUrl)
+            res.raise_for_status
+        except requests.exceptions.MissingSchema:
+            #skip this comic
+            prevLink = soup.select('a[rel="prev"]')[0]
+            url ='http://xkcd.com' + prevLink.get('href')
+            continue
 
     # TODO: Save the image to ./xkcd.
 
